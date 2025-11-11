748e84232241807c303135fa3d9afc9cc247b5d0
Ruma303 <matteoh8@hotmail.it>
2025-11-11 10:19:58 +0100

    controllers ok

--- src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package com.example.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringSecurityApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
