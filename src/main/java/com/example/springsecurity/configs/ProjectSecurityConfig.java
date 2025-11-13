cc6ea23476a12c6d330035cbb3ac095316728489
Ruma303 <matteoh8@hotmail.it>
2025-11-13 10:40:14 +0100

    form based ok

---
+++ src/main/java/com/example/springsecurity/configs/ProjectSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.springsecurity.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)
+
+                // -> requests.anyRequest().authenticated())  // autenticare ogni richiesta
+                // -> requests.anyRequest().permitAll() // accesso a tutte le richieste
+                // -> requests.anyRequest().denyAll()); // accesso negato
+                // Accesso selettivo
+                -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notice", "/contact", "/error").permitAll());
+
+        // http.formLogin(withDefaults());
+        // http.formLogin().disable(); // Vecchio metodo deprecato per disattivare il form login
+        http.formLogin(flc -> flc.disable());
+
+        // http.httpBasic(withDefaults());
+
+        http.httpBasic(htb -> htb.disable()); // Disattivazione
+        return http.build();
+    }
+}
