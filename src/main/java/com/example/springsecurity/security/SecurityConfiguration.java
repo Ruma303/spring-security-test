761c7ace4981aa72b797e62ffe364c5a31ca8e2e
Ruma303 <matteoh8@hotmail.it>
2025-11-29 16:43:37 +0100

    h2 ok

--- src/main/java/com/example/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/security/SecurityConfiguration.java
@@ -2,10 +2,8 @@ package com.example.springsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,28 +17,34 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated());
-        http.sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers -> headers
+                .frameOptions(frame -> frame.sameOrigin()));
         http.httpBasic(withDefaults());
         return http.build();
     }
 
+
     @Bean
     public UserDetailsService userDetailsService() {
-        // Creiamo un utente in memoria con nome utente "user1" e password "password1"
+        // Creiamo un utente in memoria con nome utente "user" e password "password"
         // {noop} indica che non viene utilizzata alcuna codifica della password
         UserDetails user = User.withUsername("user")
-                .password("{noop}pass")
+                .password("{noop}password")
                 .roles("USER")
+                .authorities("read")
                 .build();
 
         // Creiamo un secondo utente in memoria con nome utente "admin" e password "admin"
         UserDetails admin = User.withUsername("admin")
                 .password("{noop}admin")
                 .roles("ADMIN")
+                .authorities("edit")
                 .build();
 
         // Il costruttore accetta una lista di utenti
